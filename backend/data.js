bb93463729e92f09b9eb221385d8b03060423db8
William-Okae <okaetetewilliam@gmail.com>
2023-02-13 03:03:11 -0800

    Create-Node.JS-Server

---
+++ backend/data.js
@@ -0,0 +1,49 @@
+const data = {
+    products: [
+      {
+        name: 'Book 1',
+        slug: 'Story',
+        category: 'Story Books',
+        image: '/images/p1.jpg', // 679px × 829px
+        price: 120,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality shirt',
+      },
+      {
+        name: 'Book 2',
+        slug: 'Documentary',
+        category: 'Documentary Books',
+        image: '/images/p2.jpg',
+        price: 250,
+        countInStock: 20,
+        rating: 4.0,
+        numReviews: 10,
+        description: 'high quality product',
+      },
+      {
+        name: 'Book 3',
+        slug: 'Adventure',
+        category: 'Adventure Books',
+        image: '/images/p3.png',
+        price: 25,
+        countInStock: 15,
+        rating: 4.5,
+        numReviews: 14,
+        description: 'high quality product',
+      },
+      {
+        name: 'Book 4',
+        slug: 'Educative',
+        category: 'Educative Books',
+        image: '/images/p4.jpg',
+        price: 65,
+        countInStock: 5,
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality product',
+      },
+    ],
+  };
+  export default data;
