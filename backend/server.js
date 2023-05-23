e66dde8fc1ff81d4accbf29d780aa4dd93e21c35
William-Okae <okaetetewilliam@gmail.com>
2023-05-22 22:20:58 -0800

    Push-To-Heroku

--- backend/server.js
+++ backend/server.js
@@ -39,8 +39,8 @@ app.use('/api/orders', orderRouter);
 
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req,res) =>
-res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 );
 
 app.use((err, req, res, next) => {
